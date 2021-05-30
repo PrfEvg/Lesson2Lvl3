7e1531397b1b672e39eb6dece3cfa7924a1f96b9
PrfEvg <79623867+PrfEvg@users.noreply.github.com>
2021-05-30 13:55:58 +0300

    Delete AuthService.java

--- server/src/main/java/server/AuthService.java
+++
@@ -1,8 +0,0 @@
-package server;
-
-public interface AuthService {
-    String getNicknameByLoginAndPassword(String login, String password);
-    boolean registration(String login, String password, String nickname);
-
-    boolean changeNick(String oldNickname, String newNickname);
-}
